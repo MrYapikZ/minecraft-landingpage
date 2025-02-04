bef01222a65f5e25a555258001dabf7aad84ab86
MrYapikZ <yafialhaidar18@gmail.com>
2025-02-04 23:34:17 +0700

    update link

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -102,13 +102,15 @@ const Hero = () => {
                             Java: <b>play.lolihunter.my.id</b> <br />
                             Bedrock: IP: <b>play.lolihunter.my.id</b> | Port: <b>25171</b> <br />
                         </p>
-                        {/*<div className="mt-10 flex items-center justify-center gap-x-6">*/}
-                        {/*    <a href="#"*/}
-                        {/*       className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get*/}
-                        {/*        started</a>*/}
-                        {/*    <a href="#" className="text-sm/6 font-semibold text-gray-900">Learn more <span*/}
-                        {/*        aria-hidden="true">→</span></a>*/}
-                        {/*</div>*/}
+                        <div className="mt-10 flex items-center justify-center gap-x-6">
+                            <a href="https://discord.gg/buCST8Y5SZ"
+                               className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                Our Discord
+                            </a>
+                            <a href="https://saweria.co/MrYapikZ"
+                               className="rounded-md border-gray-300 px-3.5 py-2.5 text-sm font-semibold text-white border">Donate Us <span
+                                aria-hidden="true">→</span></a>
+                        </div>
                     </div>
                 </div>
                 <div
